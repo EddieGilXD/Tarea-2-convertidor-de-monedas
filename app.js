956709d1d3c16b4715e6fb5b6f96001a3e51b1a4
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2025-02-02 21:34:11 +0000

    trabajando en el js

---
+++ app.js
@@ -0,0 +1,10 @@
+function convertCurrency() {
+    const currency = document.getElementById("currency").value;
+    const amount = document.getElementById("amount").value;
+    
+
+    if (currency == $){
+        const yuanResult = currency * 7.19;
+        const rubloResult = currency * 98.49;
+    } else if (currency == ¥) {}
+}
